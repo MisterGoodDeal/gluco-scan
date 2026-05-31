2257f8f1e5eadc7830df4c400765d57614b1502f
Milan CAMUS <mcamus.dev@gmail.com>
2026-06-01 01:06:00 +0200

    feat: ✨ Add constants

---
+++ src/constants/api.ts
@@ -0,0 +1,4 @@
+export const OFF_BASE_URL = 'https://world.openfoodfacts.net/api/v2/product';
+export const OFF_USER_AGENT = 'carb-scanner/1.0';
+export const SCAN_COOLDOWN_MS = 2500;
+export const DEFAULT_GRAMS = 100;
